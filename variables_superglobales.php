bb7e12e9870d09eb50bdd2b5ca9f91aff53e7013
Carlos Alvarez <amcj2129@gmail.com>
2023-04-10 21:08:32 -0400

    Variables superglobales

---
+++ variables_superglobales.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * $GLOBALS
+ * $_GET
+ * $_POST
+ * $_FILES
+ * $_COOKIE
+ * $_SESSON
+ * $_REQUEST
+ * $_ENV
+ */
+
+/*  echo "<pre>";
+var_dump($_GET);
+echo "</pre>"; */
+
+function local_scope(){
+    echo "<pre>";
+    var_dump($_GET);
+    echo "</pre>";
+}
