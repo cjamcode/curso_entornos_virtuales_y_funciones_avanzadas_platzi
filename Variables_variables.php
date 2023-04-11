fa1b4d70822befaa79966ec56050656c86b8baac
Carlos Alvarez <amcj2129@gmail.com>
2023-04-10 20:51:03 -0400

    variables variables

---
+++ Variables_variables.php
@@ -0,0 +1,14 @@
+<?php
+
+/* $variable = "nombre";
+$$variable = "Mr. Michi";
+
+echo $nombre; */
+
+$nombre = "Carlos";
+$edad = 11;
+$comida_favorita = "pasta";
+
+$variable = "edad";
+
+echo $$variable;
