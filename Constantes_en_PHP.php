67f2a799be9d795b4979c5ea3c7871cd7cd82731
Carlos Alvarez <amcj2129@gmail.com>
2023-04-10 20:42:49 -0400

    Constantes en php

---
+++ Constantes_en_PHP.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/* define("MICHI", "Mr. Michi");
+define("michi", "Otro michi");
+const PI = 4.56;
+
+echo MICHI . "" .  PHP_EOL;
+echo PI . "" .  PHP_EOL; */
+
+/* $decision = true;
+
+if($decision){
+    //const BLOCK_CONSTANT_2 = "esto no se puede dentro de un bloque de codigo";
+    define("BLOCK_CONSTANT", "Esta constante fue declarada dentro de un bloque");
+
+    echo BLOCK_CONSTANT;
+} */
+
+//echo E_ERROR;
+//echo PHP_VERSION;
+//echo __LINE__;
+echo __DIR__;
